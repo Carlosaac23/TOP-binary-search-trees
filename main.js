c329fa87b5c9f9f14d57e27cc438cc9f3fe49d51
Carlos <carlosaac232001@gmail.com>
2025-10-27 19:17:10 -0500

    feat: create prettyPrint to see the tree in the console

--- main.js
+++ main.js
@@ -12,4 +12,18 @@ class Tree {
   }
 
   buildTree(array) {}
+
+  prettyPrint(node, prefix = '', isLeft = true) {
+    if (node === nul) return;
+
+    if (node.right !== null) {
+      this.prettyPrint(node.right, `${prefix}${isLeft ? '|  ' : '  '}`, false);
+    }
+
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
 }
