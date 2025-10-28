6b3060461a8f4a8397406b9dccfa889cfab1dda2
Carlos <carlosaac232001@gmail.com>
2025-10-27 19:11:28 -0500

    feat: create Node and Tree class

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hi');
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = buildTree([]);
+  }
+
+  buildTree(array) {}
+}
