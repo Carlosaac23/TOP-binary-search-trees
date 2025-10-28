2abfb2b9a3cf382c67b801506abc6306f11e961a
Carlos <carlosaac232001@gmail.com>
2025-10-28 15:49:00 -0500

    feat: - fix Tree constructor to receive an array - fix validation in prettyPrint method - create buildTree method that creates our BST

--- main.js
+++ main.js
@@ -7,14 +7,34 @@ class Node {
 }
 
 class Tree {
-  constructor() {
-    this.root = buildTree([]);
+  constructor(array) {
+    this.root = this.buildTree(array);
   }
 
-  buildTree(array) {}
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    // Clean array (Remove duplicates and order it)
+    const noDuplicateArray = [...new Set(array)];
+    const orderedArray = noDuplicateArray.sort((a, b) => a - b);
+
+    // Get the mid index and root element
+    const mid = Math.floor(orderedArray.length / 2);
+    const root = orderedArray[mid];
+
+    // Create left and right arrays
+    const leftArray = orderedArray.slice(0, mid);
+    const rightArray = orderedArray.slice(mid + 1);
+
+    // Create left and right trees
+    const leftTree = this.buildTree(leftArray);
+    const rightTree = this.buildTree(rightArray);
+
+    return new Node(root, leftTree, rightTree);
+  }
 
   prettyPrint(node, prefix = '', isLeft = true) {
-    if (node === nul) return;
+    if (node === null) return;
 
     if (node.right !== null) {
       this.prettyPrint(node.right, `${prefix}${isLeft ? '|  ' : '  '}`, false);
@@ -27,3 +47,6 @@ class Tree {
     }
   }
 }
+
+const myTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+myTree.prettyPrint();
