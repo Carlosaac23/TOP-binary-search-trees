abcef8f8212645bd83ea3e162a76ec3f5ae1a343
Carlos <carlosaac232001@gmail.com>
2025-10-29 15:49:23 -0500

    chore: testing new insert method

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ import { Tree } from './bst.js';
 
 const myTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 myTree.prettyPrint(myTree.root);
+
+// Insert a value
+myTree.insert(2);
+myTree.prettyPrint(myTree.root);
