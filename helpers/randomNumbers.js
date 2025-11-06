40c00dc96476f5cd1c1d1eb19927fbb2df3dc4cb
Carlos <carlosaac232001@gmail.com>
2025-11-06 10:44:37 -0500

    chore: delete function due to disuse

--- helpers/randomNumbers.js
+++
@@ -1,8 +0,0 @@
-export default function createArrayWithRandomNumbers(size) {
-  let numbers = [];
-  for (let i = 0; i < size; i++) {
-    const randomNumber = Math.floor(Math.random() * 100 + 1);
-    numbers.push(randomNumber);
-  }
-  return numbers;
-}
